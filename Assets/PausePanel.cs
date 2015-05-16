dd8d504680abaa7e675f9c591d87b2f47f81ea3b
GNunez <gnunez@europe.com>
2015-05-16 23:14:14 +0200

    Premier commit de la pause et Lazzie.
    Les scripts sont indépendants mais peuvent s'intégrer dans les autres si c'est plus simple. Pour tester il faut ajouter les prefab et introduire les références dans les GameObject.

---
+++ Assets/PausePanel.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PausePanel : MonoBehaviour
+{
+    Canvas canvas;
+    GUIManager gui;
+    bool pause;
+
+    string[] goalList = new string[] {
+		"Objectif en cours: distraire le public",
+		"Objectif en cours: effrayer le Capitan",
+		"Objectif en cours: séduire Colombine" 
+};
+
+    void Start()
+    {
+        gui = GameObject.Find("GUIManager").GetComponent<GUIManager>();
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+        pause = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Pause();
+        }
+    }
+    public void OnMouseOver()
+    {
+        transform.GetComponent<Highlight>().overlap = true;
+    }
+
+    public void Pause()
+    {
+        canvas.enabled = !canvas.enabled;
+
+        if (canvas.enabled)
+        {
+            gui.active = false;
+            transform.FindChild("Text").GetComponent<Text>().text = goalList[0];
+        }
+        else
+        {
+            gui.active = true;
+            transform.FindChild("Text").GetComponent<Text>().text = "";
+        }
+    
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        Camera.main.GetComponent<AudioListener>().enabled = !Camera.main.GetComponent<AudioListener>().enabled;
+
+        pause = !pause;
+    }
+
+}
