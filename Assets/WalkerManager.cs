c736ffd4c492d6218f275f3fddfec3f6107c1ef9
GrosPouletBienGras <nicolas.leroy60@outlook.fr>
2015-05-30 21:41:38 +0200

    Commit interdit
    -Modification du systeme de gens qui marchent comme des cons

---
+++ Assets/WalkerManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WalkerManager : MonoBehaviour {
+
+    public static int currentWalker = 0;
+
+    public WalkerScript walkerPrefab;
+    public Path[] _pathArray;
+    public Sprite[] _spriteArray;
+    public int _maxWalker = 1;
+    public float _timeBetweenSpawn = 1.0f;
+
+    private float _timer;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        _timer -= Time.deltaTime;
+        while (currentWalker != _maxWalker && _timer <= 0)
+        {
+            initializeWalker(Random.Range(0, _pathArray.Length), Random.Range(0, _spriteArray.Length));
+            _timer = _timeBetweenSpawn;
+            currentWalker++;
+        }
+    }
+
+    public void initializeWalker(int pathIndex, int spriteIndex)
+    {
+        WalkerScript walker = Instantiate(walkerPrefab, Vector3.zero, Quaternion.identity) as WalkerScript;
+        walker.transform.parent = this.transform;
+        walker.Initialisation();
+        walker.setSprite(_spriteArray[spriteIndex]);
+        walker.startWalking(_pathArray[pathIndex]);
+    }
+
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < _pathArray.Length; i++)
+        {
+            Gizmos.color = new Color(1, 0, 0);
+            Gizmos.DrawSphere(_pathArray[i]._start, 2f);
+            Gizmos.color = new Color(0, 0, 1);
+            Gizmos.DrawSphere(_pathArray[i]._end, 2f);
+            Gizmos.color = new Color(0.5f, 0, 0.5f);
+            Gizmos.DrawLine(_pathArray[i]._start, _pathArray[i]._end);
+        }
+    }
+}
+
+
+[System.Serializable]
+public class Path
+{
+    public Vector3 _start, _end;
+    public Path()
+    {
+        _start = Vector3.zero;
+        _end = Vector3.zero;
+    }
+    public Path(Vector3 v1, Vector3 v2)
+    {
+        _start = v1;
+        _end = v2;
+    }
+}
--- Assets/WalkerScript.cs
+++ Assets/WalkerScript.cs
@@ -4,93 +4,62 @@ using System.Collections.Generic;
 
 public class WalkerScript : MonoBehaviour {
 
-    public float moveSpeed = 1.0f;
-    public List<Path> pathList;
-    public Sprite[] spriteArray;
+    public float _moveSpeed;
     [HideInInspector]
     public bool isWalking = false;
 
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
-    private float timer;
+
+
+    public void Initialisation(){
+        _spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
+        _animator = this.GetComponentInChildren<Animator>();
+    }
 
 	// Use this for initialization
 	void Start () {
         _spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
         _animator = this.GetComponentInChildren<Animator>();
-        timer = Random.Range(0f, 2.0f);
 	}
 	
 	// Update is called once per frame
-	void Update () {      
-        if (!isWalking)
-        {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                startWalking();
-                timer = Random.Range(0f,2.0f); 
-            }
-        }
-	}
+	void Update () {
+        
+    }
 
-    public void startWalking()
+    public void setSprite(Sprite sprite)
     {
-        Debug.Log(Random.Range(0, spriteArray.Length));
-        startWalking(Random.Range(0, pathList.Count), Random.Range(0, spriteArray.Length) );
+        _spriteRenderer.sprite = sprite;
     }
-    public void startWalking(int pathIndex, int spriteIndex)
+
+
+    public void startWalking(Path path)
     {
+        this.transform.position = path._start;
+        StartCoroutine(walkingCoroutine(path));
         isWalking = true;
-        _spriteRenderer.sprite = spriteArray[spriteIndex];
-        this.transform.position = pathList[pathIndex]._start;
-        StartCoroutine(walkingCoroutine( pathList[pathIndex]));
     }
+
     private IEnumerator walkingCoroutine(Path path)
     {
         Vector3 direction = (path._end - path._start).normalized;
         while (this.transform.position != path._end)
         {
-            if (Vector3.Distance(this.transform.position, path._end) < (direction * (Time.deltaTime * moveSpeed)).magnitude )
+            if (Vector3.Distance(this.transform.position, path._end) < (direction * (Time.deltaTime * _moveSpeed)).magnitude)
             {
                 this.transform.position = path._end;
             }
-            else { 
-                this.transform.position += direction *(Time.deltaTime * moveSpeed);
+            else {
+                this.transform.position += direction * (Time.deltaTime * _moveSpeed);
             }
             yield return null;
         }
         isWalking = false;
+        WalkerManager.currentWalker--;
+        Destroy(this.gameObject);
         yield break;
     }
 
 
-    void OnDrawGizmos()
-    {
-        for (int i = 0; i < pathList.Count; i++)
-        {
-            Gizmos.color = new Color(1, 0, 0);
-            Gizmos.DrawSphere(pathList[i]._start, 2f);
-            Gizmos.color = new Color(0, 0, 1);
-            Gizmos.DrawSphere(pathList[i]._end, 2f);
-            Gizmos.color = new Color(0.5f, 0,0.5f );
-            Gizmos.DrawLine(pathList[i]._start, pathList[i]._end);
-        }
-    }
-}
-
-[System.Serializable]
-public class Path
-{
-    public Vector3 _start, _end ;
-    public Path()
-    {
-        _start = Vector3.zero;
-        _end = Vector3.zero;
-    } 
-    public Path(Vector3 v1, Vector3 v2)
-    {
-        _start = v1;
-        _end = v2;
-    }
 }
