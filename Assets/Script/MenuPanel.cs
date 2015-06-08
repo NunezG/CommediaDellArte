c123e208609aabffecacccccdbfd66b5904541a0
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-06-08 20:07:26 +0200

    Le menu affiche le pourcentage de loading

--- Assets/Script/MenuPanel.cs
+++ Assets/Script/MenuPanel.cs
@@ -8,6 +8,8 @@ public class MenuPanel : MonoBehaviour {
     public AudioClip _transitionSound;
     private AsyncOperation op;
 
+    public GameObject loadingPercentage;
+    private int percentage = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +18,7 @@ public class MenuPanel : MonoBehaviour {
 
       //  anim.Play("Fermeture");
 
-        op = Application.LoadLevelAsync(scene);
-        op.allowSceneActivation = false;
+        StartCoroutine(displayLoadingScreen(scene));
 
         ThemePlayerScript.instance.playTheme("Commedia dell'Arte Theme Principal");
 	}
@@ -27,13 +28,39 @@ public class MenuPanel : MonoBehaviour {
         StartCoroutine("startScene");
     }
 
+    IEnumerator displayLoadingScreen(string levelName)
+    {
+        //affichage de l'ecran de chargement
+       
+        //Initialisation du poucentage a 0%
+        loadingPercentage.GetComponent<UnityEngine.UI.Text>().text = percentage.ToString() + "%";
+
+        op = Application.LoadLevelAsync(levelName);
+        //op.allowSceneActivation = false;
+
+        //tant que le chargement de la scene n'est pas finis
+        while (!op.isDone)
+        {
+            //on recupere la progression du la tache
+            percentage = (int)(op.progress * 100);
+            loadingPercentage.GetComponent<UnityEngine.UI.Text>().text = percentage.ToString() + "%";
+
+            yield return null;
+        }
+        transform.FindChild("MenuButtons").gameObject.SetActive(true);
+        GetComponent<Canvas>().worldCamera = Camera.main;
+
+        loadingPercentage.SetActive(false);
+    }
+
+
     IEnumerator startScene()
     {
         this.GetComponent<AudioSource>().PlayOneShot(_transitionSound);
         ThemePlayerScript.instance.smoothThemeChange("Commedia Theme Redux", 0.5f, 2f , 3.5f);
 
         transform.FindChild("MenuButtons").gameObject.SetActive(false);
-        op.allowSceneActivation = true;
+        //op.allowSceneActivation = true;
 
         anim.Play("Ouverture");
 
