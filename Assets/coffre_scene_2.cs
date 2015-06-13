ecd50bf21a34e463b3defa6a17165c84800564d5
JeanPierreCoffe <nico@coco.com>
2015-06-13 03:31:23 +0200

    Commit preparation de la scene 2

---
+++ Assets/coffre_scene_2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coffre_scene_2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void coffreEvent(string eventName){
+		XmlManager.launchEvent(eventName, "coffre_scene_2");
+	}
+}
