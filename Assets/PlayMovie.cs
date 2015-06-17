855c24c1816258ccfb5151910ee6123c62699caf
GrosPouletBienGras <nicolas.leroy60@outlook.fr>
2015-06-17 15:33:18 +0200

    qdsfghklm

--- Assets/PlayMovie.cs
+++ Assets/PlayMovie.cs
@@ -6,15 +6,26 @@ using System.Collections;
 public class PlayMovie : MonoBehaviour {
 
 	public MovieTexture movie;
+    public float duration;
+    public string scene;
 
 	void Start () {
 		GetComponent<Renderer>().material.mainTexture = movie;
 		GetComponent<AudioSource>().clip = movie.audioClip;
 		movie.Play();
-		movie.loop = true;
-
+		//movie.loop = true;
 		GetComponent<AudioSource>().Play();
+        StartCoroutine(waitBeforeLoad());
+
 	}
+
 	void Update(){
+
 	}
+
+    IEnumerator waitBeforeLoad()
+    {
+        yield return new WaitForSeconds(duration);
+        LoadingScreen.instance.loadLevel(scene);
+    }
 }
